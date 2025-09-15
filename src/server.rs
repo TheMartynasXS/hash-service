243922ccb95cd8e0c8b8eb6701016dfaaa5c42a7
TheMartynasXS <martynasxs@proton.me>
2025-09-15 18:44:49 +0300

    menu

--- src/server.rs
+++ src/server.rs
@@ -4,18 +4,22 @@ use hash_service::hash_loader_server::{HashLoader, HashLoaderServer};
 use hash_service::{LoadRequest, LoadResponse};
 
 pub mod hash_service {
-    tonic::include_proto!("hashservice"); // The string specified here must match the proto package name
+    tonic::include_proto!("hashservice");
 }
+
+// Minimal tray setup
+use std::path::Path;
+use tray_icon::{
+    TrayIconBuilder, TrayIconEvent,
+    menu::{Menu, MenuEvent, MenuItem},
+};
 
 #[derive(Debug, Default)]
 pub struct MyHashLoader {}
 
 #[tonic::async_trait]
 impl HashLoader for MyHashLoader {
-    async fn load(
-        &self,
-        request: Request<LoadRequest>, // Accept request of type LoadRequest
-    ) -> Result<Response<LoadResponse>, Status> {
+    async fn load(&self, request: Request<LoadRequest>) -> Result<Response<LoadResponse>, Status> {
         println!("Got a request: {:?}", request);
 
         let response = LoadResponse {
@@ -28,6 +32,43 @@ impl HashLoader for MyHashLoader {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let mut menu = Menu::new();
+    let quit_item = MenuItem::new("Quit", true, None);
+
+    let icon_path = concat!(env!("CARGO_MANIFEST_DIR"), "/icon.png");
+    let icon = {
+        let image = image::open(Path::new(icon_path))
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        tray_icon::Icon::from_rgba(rgba, width, height).expect("Failed to create icon")
+    };
+
+    let menu = Menu::new();
+    menu.append(&quit_item).unwrap();
+    let tray_icon = TrayIconBuilder::new()
+        .with_menu(Box::new(menu))
+        .with_tooltip("system-tray - tray icon library!")
+        .with_icon(icon)
+        .build()
+        .unwrap();
+
+    let receiver = TrayIconEvent::receiver();
+    std::thread::spawn(move || {
+        loop {
+            match receiver.recv() {
+                Ok(event) => {
+                    println!("{:?}", event);
+                }
+                Err(e) => {
+                    eprintln!("Tray event receiver error: {:?}", e);
+                    break;
+                }
+            }
+        }
+    });
+
     let addr = "[::1]:50051".parse()?;
     let hash_loader = MyHashLoader::default();
 
